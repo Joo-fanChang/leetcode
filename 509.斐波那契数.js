13e02f641d0f18a104bddd6ac4a24c1386396071
changzhn <changzhenan0212@gmail.com>
2019-08-14 10:37:59 +0800

    更新斐波那切

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -5,15 +5,21 @@ function createFib() {
     if (cache[N]) {
       return cache[N];
     }
-    if (N === 1 || N === 2) {
+    if (N === 0 || N === 1) {
       return N;
     }
     return cache[N] = inner(N - 1) + inner(N - 2);
   }
 }
 
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
 /**
- * @param {number} n
+ * @param {number} N
  * @return {number}
  */
-var climbStairs = createFib();
+var fib = createFib();
+
