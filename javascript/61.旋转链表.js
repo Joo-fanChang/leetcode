729b8aa2f6fc34995310532b3e22993a493ae0ab
changzhn <changzhenan@kingsoft.com>
2020-08-20 09:36:45 +0800

    61 旋转链表

---
+++ javascript/61.旋转链表.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=javascript
+ *
+ * [61] 旋转链表
+ *
+ * https://leetcode-cn.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (40.52%)
+ * Likes:    319
+ * Dislikes: 0
+ * Total Accepted:    79.1K
+ * Total Submissions: 195.1K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 解释:
+ * 向右旋转 1 步: 2->0->1->NULL
+ * 向右旋转 2 步: 1->2->0->NULL
+ * 向右旋转 3 步: 0->1->2->NULL
+ * 向右旋转 4 步: 2->0->1->NULL
+ *
+ */
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+  if (!head) {
+    return head;
+  }
+  // 链表节点的个数
+  let n = 0;
+  let oldTail = head;
+  for(n = 1; oldTail.next != null; n++) {
+    oldTail = oldTail.next;
+  }
+
+  // 形成闭环
+  oldTail.next = head;
+
+  // 从头开始查找
+  let newTail = head;
+  for(let i = 1; i <= n - (k % n) - 1; i++) {
+    newTail = newTail.next;
+  }
+  head = newTail.next;
+  newTail.next = null;
+  return head;
+};
+// @lc code=end
